471cd8b5860ac66930e82a5d99c232813159e439
Joshua Richardson <joshua.richardson@myunidays.com>
2022-10-14 16:54:35 +0100

    Fixed dynamo tests

--- data-uploader/src/dynamo-client.ts
+++ data-uploader/src/dynamo-client.ts
@@ -1,5 +1,5 @@
 import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, GetCommand, GetCommandOutput } from "@aws-sdk/lib-dynamodb";
 
 export const REGION = "eu-west-1";
 export const dynamoClient = DynamoDBDocumentClient.from(new DynamoDBClient({ region: REGION }));
@@ -8,20 +8,19 @@ export async function writeCsvToDynamoDb(putItemCommand : PutItemCommand) {
     await dynamoClient.send(putItemCommand);
 }
 
-export async function readFromDynamoDb(id: string): Promise<string[]> {
+export async function readFromDynamoDb(id: string): Promise<Record<string, any>> {
     var client = DynamoDBDocumentClient.from(new DynamoDBClient({ }))
     const results = await client.send(new GetCommand({
-        TableName: 'Thing',
+        TableName: 'TestTable',
         Key: {
-            Name: id
-        }
+            Id: id
+        },
+        AttributesToGet: ['Id', 'Title', 'Description', 'DueDay', 'Owner', 'Team', 'IsAutomated', 'Resources', 'ParentId']
     }));
 
-    if(!results.Item) {
-        return new Promise(() => {
-            [""]
-        });
+    if(results == null || results.Item == null) {
+        return {};
     }
 
-    return [results.Item.toString()]
+    return results.Item
 }
--- data-uploader/tests/dynamo-db.test.ts
+++ data-uploader/tests/dynamo-db.test.ts
@@ -2,7 +2,7 @@ import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
 import { mockClient } from "aws-sdk-client-mock";
 import { readFromDynamoDb } from "../src/dynamo-client"
 
-const mockedDb = mockClient(DynamoDBDocumentClient)
+const mockedDb = mockClient(DynamoDBDocumentClient);
 
 // https://aws.amazon.com/blogs/developer/mocking-modular-aws-sdk-for-javascript-v3-in-unit-tests/
 describe('Dynamo tests', () => {
@@ -10,16 +10,47 @@ describe('Dynamo tests', () => {
         mockedDb.reset();
     });
 
-    mockedDb.on(GetCommand, {
-        TableName: 'Thing',
-        Key: { Name: 'Thing' }
-    }).resolves({
-        Item: { Name: "Thing" }
+    test('Check that the dynamo helper responds with an item', async () => {
+        mockedDb.on(GetCommand, {
+            TableName: 'TestTable',
+            Key: { Id: 'TestValue' }
+        }).resolves({
+            Item: { 
+                Id: 'TestValue',
+                Title: 'Baker',
+                Description: 'Makes yummy biscuits',
+                DueDay: 1,
+                Owner: 'Me',
+                Team: 'Bakers',
+                IsAutomated: false,
+                Resources: 'Flour, sugar, butter',
+                ParentId: ''
+            }
+        });
+        const thing = await readFromDynamoDb('TestValue') // This is the code that I have writen
+        expect(thing).toStrictEqual({"Description": "Makes yummy biscuits", "DueDay": 1, "Id": "TestValue", "IsAutomated": false, "Owner": "Me", "ParentId": "", "Resources": "Flour, sugar, butter", "Team": "Bakers", "Title": "Baker"});
     });
 
-    test('Check that dynamo actually gets the value', async () => {
-        const thing = await readFromDynamoDb('Thing') // This is the code that I have writen
-        expect(thing).toStrictEqual(["Thing"])
-    })
-})
+    test('Check that if item is unavailable that the helper returns empty', async () => {
+        mockedDb.on(GetCommand, {
+            TableName: 'TestTable',
+            Key: { Id: 'TestValue' }
+        }).resolves({
+            Item: { 
+                Id: 'TestValue',
+                Title: 'Baker',
+                Description: 'Makes yummy biscuits',
+                DueDay: 1,
+                Owner: 'Me',
+                Team: 'Bakers',
+                IsAutomated: false,
+                Resources: 'Flour, sugar, butter',
+                ParentId: ''
+            }
+        });
+
+        const thing = await readFromDynamoDb('NotTheExpectedKey');
+        expect(thing).toStrictEqual({});
+    });
+});
 
