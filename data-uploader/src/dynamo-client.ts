11d51a25812735e2f6fde72f697ee33f9df4ef31
Joshua <joshuaquarryhouse@gmail.com>
2022-10-27 22:33:34 +0100

    Can now add data to local dynamoDB

--- data-uploader/src/dynamo-client.ts
+++ data-uploader/src/dynamo-client.ts
@@ -2,7 +2,7 @@ import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, GetCommand, GetCommandOutput } from "@aws-sdk/lib-dynamodb";
 
 export const REGION = "eu-west-1";
-export const dynamoClient = DynamoDBDocumentClient.from(new DynamoDBClient({ region: REGION }));
+export const dynamoClient = DynamoDBDocumentClient.from(new DynamoDBClient({ region: REGION, endpoint: 'http://localhost:4566' }));
 
 export async function writeCsvToDynamoDb(putItemCommand : PutItemCommand) {
     await dynamoClient.send(putItemCommand);
--- populate-tables.sh
+++ populate-tables.sh
@@ -3,10 +3,10 @@ tableNames=(OnboardingTest Onboarding SomethingElse)
 tableAttributeDefinitions=(Test Live Dev)
 
 for i in ${!tableNames[@]}; do
-    filteredTableCount=$(aws dynamodb list-tables --endpoint-url=http://localhost:4566 --output text --query "length(TableNames[?@==\`${tableNames[$i]}\`])")
+    filteredTableCount=$(aws dynamodb list-tables --endpoint-url=http://localhost:4566 --region=eu-west-1 --output text --query "length(TableNames[?@==\`${tableNames[$i]}\`])")
     if [ $filteredTableCount == 0 ]; then
         echo "Create ${tableNames[$i]}"
-        aws dynamodb create-table --attribute-definitions AttributeName=${tableAttributeDefinitions[$i]},AttributeType=S --table-name ${tableNames[$i]} --key-schema AttributeName=${tableAttributeDefinitions[$i]},KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --endpoint-url http://localhost:4566
+        aws dynamodb create-table --attribute-definitions AttributeName=Id,AttributeType=S --table-name ${tableNames[$i]} --key-schema AttributeName=Id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --endpoint-url http://localhost:4566 --region=eu-west-1
     fi
     # Run the code that imports the csv and stores in the tables
 done
