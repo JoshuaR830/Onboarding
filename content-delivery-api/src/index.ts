49e3666e1462b2bbf60d1167c5dcdfbb0fb444c1
Joshua Richardson <joshua.richardson@myunidays.com>
2022-10-28 16:18:32 +0100

    Added setup for express

---
+++ content-delivery-api/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port: number = 8080;
+
+//https://developer.okta.com/blog/2018/11/15/node-express-typescript
+
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+});
+
+app.get("/", (req, res) => {
+    res.render("")
+})
